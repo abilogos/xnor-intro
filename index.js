8e93a8d9e11bc8b25ecea0df66c9561943eb97a8
Abilogos <dev.abi.log@gmail.com>
2023-09-29 00:00:34 +0300

    added cache module & fix body

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const compression = require('compression')
 const express = require('express');
 const hbs = require('express-handlebars');
 
 const app = express();
+//handlebars
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     layoutsDir: __dirname + '/views/layouts',
@@ -11,11 +13,15 @@ app.engine('hbs', hbs.engine({
 app.use(express.static('public'));
 app.set('views', __dirname + '/views/pages');
 
+if (process.env.NODE_ENV == 'production') {
+    app.use(compression());
+    app.enable('view cache');
+}
 app.get('/', (req, res) => {
     // Integrates the body of "intro.hbs" inside the defaultLayout "main.hbs"
     res.render('home');
 });
 
 //for serving
-const port = 3000;
+const port = 8009;
 app.listen(port, () => console.log(`App listening to port ${port}`));
